acd512781330558fb2d3356d569e000388361f39
Peter Kisfaludi <pkissfa@gmail.com>
2015-09-11 10:13:05 +0200

    fish

---
+++ 05-Stacks-and-queues/fish.py
@@ -0,0 +1,25 @@
+def solution(A, B):
+    # write your code in Python 2.7
+    
+    N=len(A)
+        
+    cnt=N
+    stk=[]
+    
+    for x in range(N):
+        s=A[x]
+        d=B[x]
+        
+        
+        if d==1:
+            stk.append(s)
+        else:
+            while len(stk)>0:
+                cnt-=1
+                es = stk[-1]
+                if es>s:                    
+                    break
+                else:
+                    stk.pop()
+                    
+    return cnt
