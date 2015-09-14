7e2cd56c620ec6f5170208c3fe3a6c66439bdbdb
Peter Kisfaludi <pkissfa@gmail.com>
2015-09-14 10:43:30 +0200

    nesting

---
+++ 05-Stacks-and-queues/nesting.py
@@ -0,0 +1,25 @@
+def solution(S):
+    # write your code in Python 2.7
+    
+    N=len(S)
+    
+    if N==0:
+        return 1
+        
+    if N%2!=0:
+        return 0
+        
+    cnt = 0
+    
+    for x in S:
+        if x=='(':
+            cnt+=1
+        if x==')':
+            cnt-=1
+        if cnt < 0:
+            return 0
+            
+    if cnt !=0:
+        return 0
+        
+    return 1
