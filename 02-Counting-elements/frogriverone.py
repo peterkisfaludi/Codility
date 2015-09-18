45500e88c81526c8fff117ed2f500ad58a9d3327
Peter Kisfaludi <pkissfa@gmail.com>
2015-09-18 12:58:35 +0200

    frogriverone

---
+++ 02-Counting-elements/frogriverone.py
@@ -0,0 +1,14 @@
+def solution(X, A):
+    # write your code in Python 2.7
+    
+    d=dict(zip(set(A),[0]*len(set(A))))
+    cnt=0
+    
+    for k in range(len(A)):
+        if d[A[k]]==0:
+            d[A[k]]=1
+            cnt+=1
+            if cnt==X:
+                return k
+                
+    return -1
