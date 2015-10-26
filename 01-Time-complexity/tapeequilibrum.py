56297a07c9321950b925ab815110af8ebf578c37
Peter Kisfaludi <pkissfa@gmail.com>
2015-10-26 09:05:32 +0100

    tape equlibrum

--- 01-Time-complexity/tapeequilibrum.py
+++ 01-Time-complexity/tapeequilibrum.py
@@ -1,19 +1,16 @@
-# https://codility.com/demo/results/trainingGBRVNT-Q3M/
+# https://codility.com/demo/results/trainingXG9VG4-4WN/
 
 def solution(A):
     # write your code in Python 2.7
-    sm=sum(A)
-    lsm = 0
-    rsm = sm
-    minsm = -1
     
-    for x in A[:-1]:
-        lsm+=x
-        rsm-=x
+    N=len(A)
+    
+    L = A[0]
+    R = sum(A[1:])
+    df=abs(L-R)
+    for x in A[1:-1]:
+        L+=x
+        R-=x
+        df=min(df,abs(L-R))
         
-        if minsm==-1:
-            minsm = abs(lsm - rsm)
-        else:
-            minsm = min(minsm,abs(lsm-rsm))
-            
-    return minsm
+    return df
